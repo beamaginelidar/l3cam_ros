d0f13bcae4b9990c82267466de8f366dc83fdbb8
asubirana <adria.subirana@upc.edu>
2026-01-22 15:23:41 +0100

    g_pol default to true

--- src/polarimetric_wide_stream.cpp
+++ src/polarimetric_wide_stream.cpp
@@ -60,7 +60,7 @@ int g_angle = no_angle;
 
 bool g_listening = false;
 
-bool g_pol = false; // true if polarimetric available, false if wide available
+bool g_pol = true; // true if polarimetric available, false if wide available
 bool g_stream_processed = true;
 
 cv::Mat rgbpol2rgb(const cv::Mat img, polAngle angle = no_angle)
